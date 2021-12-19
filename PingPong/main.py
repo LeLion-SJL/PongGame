1cff855aebd59f8c3749cc55d635d3458837fea4
juvitus <archisandaria@gmail.com>
2021-12-19 11:33:50 -0500

    Put in place tools and logic for ball movement

--- PingPong/main.py
+++ PingPong/main.py
@@ -7,6 +7,18 @@
 
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.vector import Vector
+
+class PongBall(Widget):
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    velocity = ReferenceListProperty(velocity_x,velocity_y)
+
+#NB: Latest Position = Current Velocity(direction and speed info) + Latst poaition, all in vector form
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
+
 
 #build the game
 class PongGame(Widget):
